6b330e3cc54c5bfb2467d1b508cce53c776a2d34
Sven Titz <sven.titz@rwth-aachen.de>
2024-02-10 23:24:23 +0100

    feat: update Nuget packages and migrate to ApplicationHostBuilder

--- ZenBotCS/Program.cs
+++ ZenBotCS/Program.cs
@@ -9,7 +9,9 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Serilog;
+using Serilog.Core;
 using System.Globalization;
 using ZenBotCS.Clients;
 using ZenBotCS.Entities;
@@ -27,89 +29,181 @@ public class Program
     {
         CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
 
-        var host = Host.CreateDefaultBuilder(args)
-            .ConfigureAppConfiguration((context, config) =>
-            {
-                config.SetBasePath(Directory.GetCurrentDirectory());
-                config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
-            })
-            .UseSerilog((hostingContext, loggerConfiguration) =>
-            {
-                loggerConfiguration.ReadFrom.Configuration(hostingContext.Configuration);
-            })
-            //.ConfigureLogging((hostingContext, logging) =>
-            //{
-            //    logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
-            //    logging.AddConsole();
-            //    logging.AddDebug();
-            //})
-            .ConfigureDiscordHost((context, config) =>
-            {
-                config.SocketConfig = new DiscordSocketConfig
-                {
-                    LogLevel = LogSeverity.Info,
-                    AlwaysDownloadUsers = true,
-                    MessageCacheSize = 200,
-                    GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.MessageContent
-                };
-                config.Token = context.Configuration["DiscordToken"]!;
-            })
-            .ConfigureCocApi((context, services, options) =>
-            {
-                options.AddTokens(new ApiKeyToken(context.Configuration["CocApiToken"]!, ClientUtils.ApiKeyHeader.Authorization));
-                options.AddCocApiHttpClients(
-                    builder: builder => builder
-                        .AddRetryPolicy(3)
-                        .AddTimeoutPolicy(TimeSpan.FromMilliseconds(3000))
-                        .AddCircuitBreakerPolicy(30, TimeSpan.FromSeconds(10))
-                    );
-            })
-            .UseInteractionService((context, config) =>
-            {
-                config.LogLevel = LogSeverity.Info;
-                config.UseCompiledLambda = true;
-            })
-            .ConfigureServices((context, services) =>
-            {
-                services.AddHostedService<InteractionHandler>();
-                services.AddHostedService<DiscordLinkUpdateService>();
-                services.AddHostedService<WarHistoryUpdateService>();
-                services.AddTransient<TestService>();
-                services.AddTransient<GspreadService>();
-                services.AddTransient<PlayerService>();
-                services.AddTransient<ClanService>();
-                services.AddTransient<CwlService>();
-                services.AddTransient<HelpService>();
-                services.AddTransient<ClashKingApiClient>();
-                services.AddTransient<EmbedHelper>();
-                services.AddTransient<DiscordHelper>();
-                services.AddDbContext<BotDataContext>(options =>
-                    {
-                        var connectionString = context.Configuration["BotDbConnectionString"]!;
-                        options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString), b => b.MigrationsAssembly("ZenBotCS"));
+        var builder = Host.CreateApplicationBuilder(args);
 
-                    });
-                services.AddCocApiCache<CustomClansClient, PlayersClient, TimeToLiveProvider>(
-                    dbContextOptions =>
-                    {
-                        var connectionString = context.Configuration["CocApiCacheConnectionString"]!;
-                        dbContextOptions.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString), b => b.MigrationsAssembly("ZenBotCS"));
-                        CacheDbContext dbContext = new((DbContextOptions<CacheDbContext>)dbContextOptions.Options);
-                    },
-                    options =>
-                    {
-                        options.ActiveWars.Enabled = true;
-                        options.ClanMembers.Enabled = true;
-                        options.Clans.Enabled = true;
-                        options.CwlWars.Enabled = true;
-                        options.NewCwlWars.Enabled = true;
-                        options.NewWars.Enabled = true;
-                        options.Players.Enabled = true;
-                        options.Wars.Enabled = true;
-                    });
-            })
-            .Build();
+        builder.Configuration
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+        builder.Services.AddLogging(config =>
+        {
+            config.ClearProviders();
+
+            Logger logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(builder.Configuration)
+                .CreateLogger();
+
+            config.AddSerilog(logger);
+        });
+
+        builder.Services.AddDiscordHost((config, _) =>
+        {
+            config.SocketConfig = new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Info,
+                AlwaysDownloadUsers = true,
+                MessageCacheSize = 200,
+                GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.MessageContent
+            };
+            config.Token = builder.Configuration["DiscordToken"]!;
+        });
+
+        builder.Services.AddCocApi(options =>
+        {
+            options.AddTokens(new ApiKeyToken(builder.Configuration["CocApiToken"]!, ClientUtils.ApiKeyHeader.Authorization));
+            options.AddCocApiHttpClients(
+                builder: builder => builder
+                    .AddRetryPolicy(3)
+                    .AddTimeoutPolicy(TimeSpan.FromMilliseconds(3000))
+                    .AddCircuitBreakerPolicy(30, TimeSpan.FromSeconds(10))
+                );
+        });
+
+        builder.Services.AddInteractionService((config, _) =>
+        {
+            config.LogLevel = LogSeverity.Info;
+            config.UseCompiledLambda = true;
+        });
+
+        builder.Services
+            .AddHostedService<InteractionHandler>()
+            .AddHostedService<DiscordLinkUpdateService>()
+            .AddHostedService<WarHistoryUpdateService>()
+            .AddTransient<TestService>()
+            .AddTransient<GspreadService>()
+            .AddTransient<PlayerService>()
+            .AddTransient<ClanService>()
+            .AddTransient<CwlService>()
+            .AddTransient<HelpService>()
+            .AddTransient<ClashKingApiClient>()
+            .AddTransient<EmbedHelper>()
+            .AddTransient<DiscordHelper>();
+
+
+        builder.Services.AddDbContext<BotDataContext>(options =>
+        {
+            var connectionString = builder.Configuration["BotDbConnectionString"]!;
+            options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString), b => b.MigrationsAssembly("ZenBotCS"));
+
+        });
+        builder.Services.AddCocApiCache<CustomClansClient, PlayersClient, TimeToLiveProvider>(
+            dbContextOptions =>
+            {
+                var connectionString = builder.Configuration["CocApiCacheConnectionString"]!;
+                dbContextOptions.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString), b => b.MigrationsAssembly("ZenBotCS"));
+                CacheDbContext dbContext = new((DbContextOptions<CacheDbContext>)dbContextOptions.Options);
+            },
+            options =>
+            {
+                options.ActiveWars.Enabled = true;
+                options.ClanMembers.Enabled = true;
+                options.Clans.Enabled = true;
+                options.CwlWars.Enabled = true;
+                options.NewCwlWars.Enabled = true;
+                options.NewWars.Enabled = true;
+                options.Players.Enabled = true;
+                options.Wars.Enabled = true;
+            });
+
+        var host = builder.Build();
 
         await host.RunAsync();
+
+
+
+        //var host2 = Host.CreateDefaultBuilder(args)
+        //    .ConfigureAppConfiguration((context, config) =>
+        //    {
+        //        config.SetBasePath(Directory.GetCurrentDirectory());
+        //        config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+        //    })
+        //    .UseSerilog((hostingContext, loggerConfiguration) =>
+        //    {
+        //        loggerConfiguration.ReadFrom.Configuration(hostingContext.Configuration);
+        //    })
+        //    //.ConfigureLogging((hostingContext, logging) =>
+        //    //{
+        //    //    logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+        //    //    logging.AddConsole();
+        //    //    logging.AddDebug();
+        //    //})
+        //    .ConfigureDiscordHost((context, config) =>
+        //    {
+        //        config.SocketConfig = new DiscordSocketConfig
+        //        {
+        //            LogLevel = LogSeverity.Info,
+        //            AlwaysDownloadUsers = true,
+        //            MessageCacheSize = 200,
+        //            GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.MessageContent
+        //        };
+        //        config.Token = context.Configuration["DiscordToken"]!;
+        //    })
+        //    .ConfigureCocApi((context, services, options) =>
+        //    {
+        //        options.AddTokens(new ApiKeyToken(context.Configuration["CocApiToken"]!, ClientUtils.ApiKeyHeader.Authorization));
+        //        options.AddCocApiHttpClients(
+        //            builder: builder => builder
+        //                .AddRetryPolicy(3)
+        //                .AddTimeoutPolicy(TimeSpan.FromMilliseconds(3000))
+        //                .AddCircuitBreakerPolicy(30, TimeSpan.FromSeconds(10))
+        //            );
+        //    })
+        //    .UseInteractionService((context, config) =>
+        //    {
+        //        config.LogLevel = LogSeverity.Info;
+        //        config.UseCompiledLambda = true;
+        //    })
+        //    .ConfigureServices((context, services) =>
+        //    {
+        //        services.AddHostedService<InteractionHandler>();
+        //        services.AddHostedService<DiscordLinkUpdateService>();
+        //        services.AddHostedService<WarHistoryUpdateService>();
+        //        services.AddTransient<TestService>();
+        //        services.AddTransient<GspreadService>();
+        //        services.AddTransient<PlayerService>();
+        //        services.AddTransient<ClanService>();
+        //        services.AddTransient<CwlService>();
+        //        services.AddTransient<HelpService>();
+        //        services.AddTransient<ClashKingApiClient>();
+        //        services.AddTransient<EmbedHelper>();
+        //        services.AddTransient<DiscordHelper>();
+        //        services.AddDbContext<BotDataContext>(options =>
+        //            {
+        //                var connectionString = context.Configuration["BotDbConnectionString"]!;
+        //                options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString), b => b.MigrationsAssembly("ZenBotCS"));
+
+        //            });
+        //        services.AddCocApiCache<CustomClansClient, PlayersClient, TimeToLiveProvider>(
+        //            dbContextOptions =>
+        //            {
+        //                var connectionString = context.Configuration["CocApiCacheConnectionString"]!;
+        //                dbContextOptions.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString), b => b.MigrationsAssembly("ZenBotCS"));
+        //                CacheDbContext dbContext = new((DbContextOptions<CacheDbContext>)dbContextOptions.Options);
+        //            },
+        //            options =>
+        //            {
+        //                options.ActiveWars.Enabled = true;
+        //                options.ClanMembers.Enabled = true;
+        //                options.Clans.Enabled = true;
+        //                options.CwlWars.Enabled = true;
+        //                options.NewCwlWars.Enabled = true;
+        //                options.NewWars.Enabled = true;
+        //                options.Players.Enabled = true;
+        //                options.Wars.Enabled = true;
+        //            });
+        //    })
+        //    .Build();
+
+        //await host2.RunAsync();
     }
 }
